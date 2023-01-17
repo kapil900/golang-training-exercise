3c3f2f448e3f978fba7a49fccac9b3171fd30b2a
Raramuri <kapil.kadian@zopsmart.com>
2023-01-17 14:24:37 +0530

    Resolving issue 1

---
+++ StringUtil/stringUtil.go
@@ -0,0 +1,38 @@
+package stringUtil
+
+import (
+	"fmt"
+	"time"
+)
+
+func CommonString(a, b string) string {
+	start := time.Now()
+
+	defer fmt.Println(time.Now().Format(time.RFC850))
+
+	solution := ""
+	count := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] == b[0] {
+			for j := 0; j < len(b); j++ {
+
+				if a[i+j] != b[j] {
+					count = 0
+					break
+				} else {
+					count++
+					solution += string(a[i+j])
+				}
+			}
+			if count == len(b) {
+				break
+			} else {
+				solution = ""
+			}
+		}
+	}
+	end := time.Since(start)
+	fmt.Println(end)
+
+	return solution
+}
---
+++ StringUtil/stringUtil_test.go
@@ -0,0 +1,70 @@
+package stringUtil
+
+import "testing"
+
+func TestCommonString1(t *testing.T) {
+
+	input1 := "ahelloworld"
+	input2 := "hello"
+	actualString := CommonString(input1, input2)
+
+	expectedString := "hello"
+	if actualString != expectedString {
+		t.Errorf("Expected String %s is not same as"+"actual string %s .", expectedString, actualString)
+	}
+}
+func TestCommonString2(t *testing.T) {
+
+	input1 := "my"
+	input2 := "MY"
+	actualString := CommonString(input1, input2)
+
+	expectedString := "my"
+	if actualString != expectedString {
+		t.Errorf("Expected String %s is not same as"+"actual string %s .", expectedString, actualString)
+	}
+}
+func TestCommonString3(t *testing.T) {
+
+	input1 := ""
+	input2 := "he"
+	actualString := CommonString(input1, input2)
+
+	expectedString := ""
+	if actualString != expectedString {
+		t.Errorf("Expected String %s is not same as"+"actual string %s .", expectedString, actualString)
+	}
+}
+func TestCommonString4(t *testing.T) {
+
+	input1 := ""
+	input2 := ""
+	actualString := CommonString(input1, input2)
+
+	expectedString := "l"
+	if actualString != expectedString {
+		t.Errorf("Expected String %s is not same as"+"actual string %s .", expectedString, actualString)
+	}
+}
+func TestCommonString5(t *testing.T) {
+
+	input1 := "ahellow"
+	input2 := "hello"
+	actualString := CommonString(input1, input2)
+
+	expectedString := "ahellow"
+	if actualString != expectedString {
+		t.Errorf("Expected String %s is not same as"+"actual string %s .", expectedString, actualString)
+	}
+}
+func TestCommonString6(t *testing.T) {
+
+	input1 := "done"
+	input2 := "done"
+	actualString := CommonString(input1, input2)
+
+	expectedString := "done"
+	if actualString != expectedString {
+		t.Errorf("Expected String %s is not same as"+"actual string %s .", expectedString, actualString)
+	}
+}
